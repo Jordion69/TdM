8ad87a4903503b5143084988cc0779299bad20ef
Jordion69 <jortiznavas0@gmail.com>
2023-04-13 07:54:04 +0200

    noticias

---
+++ src/app/modules/garitos/components/main-cities/main-cities.component.ts
@@ -0,0 +1,10 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-cities',
+  templateUrl: './main-cities.component.html',
+  styleUrls: ['./main-cities.component.scss']
+})
+export class MainCitiesComponent  {
+
+}
--- src/app/modules/garitos/garitos.module.ts
+++ src/app/modules/garitos/garitos.module.ts
@@ -5,13 +5,15 @@ import { GaritosRoutingModule } from './garitos-routing.module';
 import { PageComponent } from './page/page.component';
 import { SearchComponent } from './components/search/search.component';
 import { CardComponent } from './components/card/card.component';
+import { MainCitiesComponent } from './components/main-cities/main-cities.component';
 
 
 @NgModule({
   declarations: [
     PageComponent,
     SearchComponent,
-    CardComponent
+    CardComponent,
+    MainCitiesComponent
   ],
   imports: [
     CommonModule,
