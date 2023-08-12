55b663530cb3de7831da2131062d52725f4320ad
Jordion69 <jortiznavas0@gmail.com>
2023-08-12 14:08:46 +0200

    primer commit contacto

--- src/app/modules/conciertos/components/card/card.component.ts
+++ src/app/modules/conciertos/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -16,5 +16,26 @@ export class CardComponent {
     { id: 6, bandaAux: "Ratas + 11Bis", day: "22", month: "Marzo", src: "../../../../../assets/img/conciertos_test.jpg", dia: 30, mes: "Marzo", ciudad: "Barcelona", Lugar: "Sala Razzmatazz", paginaEntradas: "https://www.madnesslive.es/es/conciertos-en-barcelona/835-comprar-entrada-wasp-barcelona.html", año: 2023, banda: "Legion"},
     { id: 7, bandaAux: "Ratas + 11Bis", day: "24", month: "Mayo", src: "../../../../../assets/img/conciertos_test.jpg", dia: 31, mes: "Marzo", ciudad: "Barcelona", Lugar: "Sala Razzmatazz", paginaEntradas: "https://www.madnesslive.es/es/conciertos-en-barcelona/835-comprar-entrada-wasp-barcelona.html", año: 2023, banda: "Sonata Arctica" },
     { id: 8, bandaAux: "Ratas + 11Bis", day: "18", month: "Marzo", src: "../../../../../assets/img/conciertos_test.jpg", dia: 1, mes: "Abril", ciudad: "Barcelona", Lugar: "Sala Razzmatazz", paginaEntradas: "https://www.madnesslive.es/es/conciertos-en-barcelona/835-comprar-entrada-wasp-barcelona.html", año: 2023, banda: "Obituary/guns.jpg" }
-  ]
+  ];
+  modalImage: string = '';
+
+  openModal(imageSrc: string) {
+    this.modalImage = imageSrc;
+    $('#gallery-modal').on('show.bs.modal', () => {
+      const img = new Image();
+      img.src = imageSrc;
+      img.onload = () => {
+        const modal = $('#gallery-modal .modal-dialog');
+        const maxHeight = window.innerHeight - 40; // 40px para el espacio alrededor
+        if (img.height > maxHeight) {
+          modal.css('max-height', `${maxHeight}px`);
+          modal.css('overflow-y', 'scroll');
+        } else {
+          modal.css('max-height', 'none');
+          modal.css('overflow-y', 'auto');
+        }
+      };
+    });
+    $('#gallery-modal').modal('show');
+  }
 }
--- src/app/modules/contacto/contacto.module.ts
+++ src/app/modules/contacto/contacto.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContactoRoutingModule } from './contacto-routing.module';
 import { PageComponent } from './page/page.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 
 @NgModule({
   declarations: [
-    PageComponent
+    PageComponent,
+    FormularioComponent
   ],
   imports: [
     CommonModule,
